238520e95b4aff1d07bc422612a30da4eb042e46
Alexandre Daood Thomspon <alexandre.daood-thompson@epita.fr>
2019-10-21 18:35:04 +0200

    Quelques corrections mineures de coding style ;)

--- Binairisation/monochrom.c
+++ Binairisation/monochrom.c
@@ -12,11 +12,11 @@ void init_sdl()
 {
     // Init only the video part.
     // If it fails, die with an error message.
-    if(SDL_Init(SDL_INIT_VIDEO) == -1)
-        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) == -1)
+        errx(1, "Could not initialize SDL: %s.\n", SDL_GetError());
 }
 
-SDL_Surface* load_image(char *path)
+SDL_Surface *load_image(char *path)
 {
     SDL_Surface *img;
 
@@ -29,21 +29,21 @@ SDL_Surface* load_image(char *path)
     return img;
 }
 
-SDL_Surface* display_image(SDL_Surface *img)
+SDL_Surface *display_image(SDL_Surface *img)
 {
     SDL_Surface *screen;
 
     // Set the window to the same size as the image
-    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE | SDL_ANYFORMAT);
     if (screen == NULL)
     {
         // error management
         errx(1, "Couldn't set %dx%d video mode: %s\n",
-             img->w, img->h, SDL_GetError());
+        img->w, img->h, SDL_GetError());
     }
 
     // Blit onto the screen surface
-    if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    if (SDL_BlitSurface(img, NULL, screen, NULL) < 0)
         warnx("BlitSurface error: %s\n", SDL_GetError());
 
     // Update the screen
@@ -61,13 +61,13 @@ void wait_for_keypressed()
     do
     {
         SDL_PollEvent(&event);
-    } while(event.type != SDL_KEYDOWN);
+    } while (event.type != SDL_KEYDOWN);
 
     // Wait for a key to be up.
     do
     {
         SDL_PollEvent(&event);
-    } while(event.type != SDL_KEYUP);
+    } while (event.type != SDL_KEYUP);
 }
 
 void grayscale(SDL_Surface *image)
@@ -86,18 +86,17 @@ void grayscale(SDL_Surface *image)
     {
         for (x = 0; x < w; x++)
         {
-            pixel = get_pixel(image,x,y);
+            pixel = get_pixel(image, x, y);
             SDL_GetRGBA(pixel, image->format, &r, &g, &b, &a);
-            Uint8 new_color = (0.3*r+0.59*g+0.11*b);
-            pixel=SDL_MapRGBA(image->format, new_color, new_color, new_color, a);
-            put_pixel(image,x,y,pixel);
+            Uint8 new_color = (0.3 * r + 0.59 * g + 0.11 * b);
+            pixel = SDL_MapRGBA(image->format, new_color, new_color, new_color, a);
+            put_pixel(image, x, y, pixel);
         }
-
     }
     SDL_UnlockSurface(image);
 }
 
-int* Create_Matrix(SDL_Surface *image)
+int *Create_Matrix(SDL_Surface *image)
 {
     SDL_LockSurface(image);
     Uint32 pixel;
@@ -110,43 +109,43 @@ int* Create_Matrix(SDL_Surface *image)
     unsigned long y;
     unsigned long h = image->h;
     unsigned long w = image->w;
-    int size = h*w;
-    int matrix [size];
-    for (int i = 0; i < size; ++i) {
+    int size = h * w;
+    int matrix[size];
+    for (int i = 0; i < size; ++i)
+    {
         matrix[i] = 0;
     }
     for (y = 0; y < h; y++)
     {
         for (x = 0; x < w; x++)
         {
-            pixel = get_pixel(image,x,y);
+            pixel = get_pixel(image, x, y);
             SDL_GetRGBA(pixel, image->format, &r, &g, &b, &a);
             if (r == 0)
             {
-                matrix[i]= 1;
+                matrix[i] = 1;
             }
             else
             {
-                matrix[i]=0;
+                matrix[i] = 0;
             }
             i++;
         }
-
     }
-    for (int i = 0; i<size;i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%d",matrix[i]);
-        if((i+1)%w ==0)
+        printf("%d", matrix[i]);
+        if ((i + 1) % w == 0)
         {
             printf("\n");
         }
     }
     SDL_UnlockSurface(image);
-    int* PointerToMatrix = matrix;
+    int *PointerToMatrix = matrix;
     return PointerToMatrix;
 }
 
-void black_white (SDL_Surface *image)
+void black_white(SDL_Surface *image)
 {
     SDL_LockSurface(image);
     Uint32 pixel;
@@ -162,31 +161,28 @@ void black_white (SDL_Surface *image)
     {
         for (x = 0; x < w; x++)
         {
-            pixel = get_pixel(image,x,y);
+            pixel = get_pixel(image, x, y);
             SDL_GetRGBA(pixel, image->format, &r, &g, &b, &a);
             Uint8 new_color;
-            if (r<=127)
+            if (r <= 127)
             {
-                new_color=0;
-            } else
-            {
-                new_color =255;
+                new_color = 0;
             }
-            pixel=SDL_MapRGBA(image->format, new_color, new_color, new_color, a);
-            put_pixel(image,x,y,pixel);
+            else
+            {
+                new_color = 255;
+            }
+            pixel = SDL_MapRGBA(image->format, new_color, new_color, new_color, a);
+            put_pixel(image, x, y, pixel);
         }
-
     }
     SDL_UnlockSurface(image);
 }
 
-
-
-
 int main()
 {
-    SDL_Surface* image_surface;
-    SDL_Surface* screen_surface;
+    SDL_Surface *image_surface;
+    SDL_Surface *screen_surface;
 
     init_sdl();
     image_surface = load_image("my_image.jpg");
@@ -196,12 +192,11 @@ int main()
     update_surface(screen_surface, image_surface);
     wait_for_keypressed();
     black_white(image_surface);
-    update_surface(screen_surface,image_surface);
+    update_surface(screen_surface, image_surface);
     Create_Matrix(image_surface);
     wait_for_keypressed();
     SDL_FreeSurface(image_surface);
     SDL_FreeSurface(screen_surface);
-
 
     return 0;
 }
